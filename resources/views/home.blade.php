c67b00c88080c07eab32b89ebd97046892d5c468
marlinapuspitadewi29 <marlina_173040073@mail.unpas.ac.id>
2021-07-14 14:46:39 +0700

    menambahkan file post.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>Halaman Home</h1>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+//use PhpParser\Node\Stmt\Return_;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Marlina Puspita Dewi",
+        "email" => "marlina_173040073@mail.unpas.ac.id",
+        "image" => "marlina.jpg"
+
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => Post::all()
+
+    ]);
+});
+
+
+//halaman single post
+Route::get('/posts/{slug}', function ($slug) {
+
+
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => Post::find($slug)
+    ]);
+});
