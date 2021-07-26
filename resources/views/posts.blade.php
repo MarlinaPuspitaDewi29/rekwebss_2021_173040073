f72c32b6665e3887d9fbe3dbbc155af99094e54f
marlinapuspitadewi29 <marlina_173040073@mail.unpas.ac.id>
2021-07-26 17:59:53 +0700

    menambahkan class file posts.blade.php pada bagian blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,8 +4,11 @@
     <h1 class="mb-5">Halaman Blog Posts</h1>
 
 @foreach ($posts as $post)
-<article class="mb-5">
-    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+<article class="mb-5 border-bottom">
+    <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+
+    <p>By. Marlina Puspita Dewi in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    
     <p>{{ $post->excerpt }}</p>
 </article>
 @endforeach
